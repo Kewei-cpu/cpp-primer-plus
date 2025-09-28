2c5837552c0e4339d67be2399c56f260c2ad349b
Keweijun <zkw42dsz@sina.com>
2025-09-28 22:42:34 +0800

    fix Chap9

--- Chap9/4_im.cpp
+++ Chap9/4_im.cpp
@@ -1,45 +1,50 @@
-// #pragma once
+#pragma once
 #include <iostream>
 #include "4_ns.h"
 
-
-inline void SALES::setSales(Sales& s, const double ar[], int n)
+namespace SALES
 {
-    for (int i = 0; i < std::min(QUARTERS, n); i++)
+    void setSales(Sales& s, const double ar[], int n)
     {
-        const double sales = ar[i];
+        s.min = LONG_LONG_MAX;
+        s.max = LONG_LONG_MIN;
+        s.average = 0.0;
 
-        if (sales > s.max) s.max = sales;
-        if (sales < s.max) s.min = sales;
-        s.average = (s.average * (i - 1) + sales) / i;
-    }
-}
-
-inline void SALES::setSales(Sales& s)
-{
-    std::cout << "Enter your sales" << std::endl;
-
-    int n;
-    double ar[4];
-    for (n = 0; n < QUARTERS; n++)
-    {
-        std::cout << "Q" << n + 1 << ": ";
-        std::cin >> ar[n];
+        for (int i = 0; i < std::min(QUARTERS, n); i++)
+        {
+            const double sales = ar[i];
+            s.sales[i] = sales;
+            if (sales > s.max) s.max = sales;
+            if (sales < s.min) s.min = sales;
+            s.average = (s.average * i + sales) / (i + 1);
+        }
     }
 
-    setSales(s, ar, n);
-}
-
-inline void SALES::showSales(const Sales& s)
-{
-    std::cout << "Sales: " << std::endl;
-    for (int i = 0; i < QUARTERS; i++)
+    void setSales(Sales& s)
     {
-        std::cout << "Q" << i + 1 << ": ";
-        std::cout << s.sales[i] <<std::endl;
-    }
-    std::cout << "Min: " << s.min;
-    std::cout << "Max: " << s.max;
-    std::cout << "Avg: " << s.average;
-}
+        std::cout << "Enter your sales" << std::endl;
 
+        int n;
+        double ar[4];
+        for (n = 0; n < QUARTERS; n++)
+        {
+            std::cout << "Q" << n + 1 << ": ";
+            std::cin >> ar[n];
+        }
+
+        setSales(s, ar, n + 1);
+    }
+
+    void showSales(const Sales& s)
+    {
+        std::cout << "Sales: " << std::endl;
+        for (int i = 0; i < QUARTERS; i++)
+        {
+            std::cout << "Q" << i + 1 << ": ";
+            std::cout << s.sales[i] << std::endl;
+        }
+        std::cout << "Min: " << s.min << " | ";
+        std::cout << "Max: " << s.max << " | ";
+        std::cout << "Avg: " << s.average << std::endl;
+    }
+}
